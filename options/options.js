f1d4ff63cc834eab5abfa2f733c38ce40ec70250
Crowdbotics <team@crowdbotics.com>
2023-02-08 16:22:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "onlinemoneyking-38815",
+  url: "https://onlinemoneyking-38815.botics.co",
+  api: "https://onlinemoneyking-38815.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
